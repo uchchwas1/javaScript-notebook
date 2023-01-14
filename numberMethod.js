2150acfb9d9de8ae9e1884fd7620e05f798766cc
Uchchwas <uchchwas.das@northsouth.edu>
2023-01-15 00:57:26 +0600

    number method

---
+++ numberMethod.js
@@ -0,0 +1,9 @@
+//number method
+var i = 20;
+var f = parseFloat(i);
+var s = toString(i);
+
+console.log(f);
+console.log("Float: "+typeof(f));
+console.log("number: "+typeof(i));
+console.log("string: "+typeof(s));
